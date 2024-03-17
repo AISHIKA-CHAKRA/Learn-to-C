7fec2dbbee7292d99ebe9ea634e83c2fa9a2eeef
AISHIKA-CHAKRA <aishikachakra2@gmail.com>
2024-03-17 20:53:05 +0530

    3rd commit

---
+++ Learn-to-C/Codechef/Course/LBCL04_problems_CLB018.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	printf("%d\n", 9 - 2);// Print the result of 9 - 2
+	
+	printf("%d\n", 30/5);// Print the result of 30 / 5
+	
+	return 0;
+}
---
+++ Learn-to-C/Codechef/Course/LBCL05_problems_CLB024.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	// Assign the value 23 to a
+int a =23, b = 20;
+    
+    // Assign the value 20 to b
+printf("%d", a + b);
+    
+    // Output the sum of a and b
+}
